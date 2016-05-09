6ada5d2256d55dccab431fb902c0d5bcca536080
nicolette <nicolette.williams8@icloud.com>
2016-05-09 09:36:17 -0500

    combined 2 queries to find all employees that were hired in the 90s and born on Christmas. rewrote last query to find all employees with a q in their last names but not 'qu'.

--- where_exercise.sql
+++ where_exercise.sql
@@ -15,14 +15,12 @@ FROM employees
 WHERE (last_name LIKE 'E%'
 AND last_name LIKE '%E');
 
-SELECT first_name, last_name, hire_date
-from employees
-where hire_date BETWEEN '1990-01-01' and '1999-12-31';
-
 SELECT first_name, last_name, birth_date
 FROM employees
-WHERE birth_date LIKE '%-12-25';
+WHERE birth_date LIKE '%-12-25'
+and hire_date BETWEEN '1990-01-01' and '1999-12-31';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name NOT LIKE '%qu%'
+AND last_name LIKE '%q%';
