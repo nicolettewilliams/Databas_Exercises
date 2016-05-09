47d82d05a6a0246eaca7a5d31432734d1c8b0155
nicolette <nicolette.williams8@icloud.com>
2016-05-09 10:45:25 -0500

    sorted by employee number

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -6,3 +6,9 @@ OR first_name = 'Maya')
 AND gender = 'M'
 ORDER BY last_name, first_name;
 
+SELECT concat(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE (last_name LIKE 'E%'
+OR last_name LIKE '%E')
+ORDER BY emp_no;
+
